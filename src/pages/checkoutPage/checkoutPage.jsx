4613416a3d105ee73a9824a3919e8c2ac181bd85
June Yun <97yunjy@gmail.com>
2026-07-20 21:49:30 -0600

    implement checkout user inputs

--- src/pages/checkoutPage/checkoutPage.jsx
+++ src/pages/checkoutPage/checkoutPage.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import Header from "../../components/layout/header";
 import Footer from "../../components/layout/footer";
 import "./checkoutPage.css";
 
 const CheckoutPage = () => {
+  const [shippingInfo, setShippingInfo] = useState({
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    province: "",
+    postalCode: "",
+  });
+
+  const handleShippingChange = (event) => {
+    const { name, value } = event.target;
+
+    setShippingInfo((prevShippingInfo) => ({
+      ...prevShippingInfo,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="checkout-page">
       <Header />
@@ -19,10 +38,110 @@ const CheckoutPage = () => {
               <p>2 items</p>
             </div>
 
-            <div className="checkout-page__placeholder">
+            <section className="checkout-page__shipping-section">
               <h3>Shipping Address</h3>
-              <p>Shipping and payment forms will be added here.</p>
-            </div>
+
+              <form className="checkout-page__shipping-form">
+                <div className="checkout-page__form-row">
+                  <div className="checkout-page__field">
+                    <label htmlFor="firstName">First Name</label>
+
+                    <input
+                      id="firstName"
+                      name="firstName"
+                      type="text"
+                      value={shippingInfo.firstName}
+                      onChange={handleShippingChange}
+                      placeholder="First Name"
+                      autoComplete="given-name"
+                    />
+                  </div>
+
+                  <div className="checkout-page__field">
+                    <label htmlFor="lastName">Last Name</label>
+
+                    <input
+                      id="lastName"
+                      name="lastName"
+                      type="text"
+                      value={shippingInfo.lastName}
+                      onChange={handleShippingChange}
+                      placeholder="Last Name"
+                      autoComplete="family-name"
+                    />
+                  </div>
+                </div>
+
+                <div className="checkout-page__field">
+                  <label htmlFor="address">Address</label>
+
+                  <input
+                    id="address"
+                    name="address"
+                    type="text"
+                    value={shippingInfo.address}
+                    onChange={handleShippingChange}
+                    placeholder="Address"
+                    autoComplete="street-address"
+                  />
+                </div>
+
+                <div className="checkout-page__field">
+                  <label htmlFor="city">City</label>
+
+                  <input
+                    id="city"
+                    name="city"
+                    type="text"
+                    value={shippingInfo.city}
+                    onChange={handleShippingChange}
+                    placeholder="City"
+                    autoComplete="address-level2"
+                  />
+                </div>
+
+                <div className="checkout-page__form-row">
+                  <div className="checkout-page__field">
+                    <label htmlFor="province">Province</label>
+
+                    <select
+                      id="province"
+                      name="province"
+                      value={shippingInfo.province}
+                      onChange={handleShippingChange}
+                      autoComplete="address-level1"
+                    >
+                      <option value="">Select Province</option>
+                      <option value="AB">Alberta</option>
+                      <option value="BC">British Columbia</option>
+                      <option value="MB">Manitoba</option>
+                      <option value="NB">New Brunswick</option>
+                      <option value="NL">Newfoundland and Labrador</option>
+                      <option value="NS">Nova Scotia</option>
+                      <option value="ON">Ontario</option>
+                      <option value="PE">Prince Edward Island</option>
+                      <option value="QC">Quebec</option>
+                      <option value="SK">Saskatchewan</option>
+                    </select>
+                  </div>
+
+                  <div className="checkout-page__field">
+                    <label htmlFor="postalCode">Postal Code</label>
+
+                    <input
+                      id="postalCode"
+                      name="postalCode"
+                      type="text"
+                      value={shippingInfo.postalCode}
+                      onChange={handleShippingChange}
+                      placeholder="Postal Code"
+                      autoComplete="postal-code"
+                      maxLength={7}
+                    />
+                  </div>
+                </div>
+              </form>
+            </section>
           </div>
 
           <aside className="checkout-page__summary-card">
